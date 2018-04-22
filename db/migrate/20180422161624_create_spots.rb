e6b5686a78dc78a67234d6fe1bf4113fb9200de5
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-22 11:40:50 -0500

    added migrations and dumped in csv file

--- db/migrate/20180422161624_create_spots.rb
+++ db/migrate/20180422161624_create_spots.rb
@@ -1,6 +1,11 @@
 class CreateSpots < ActiveRecord::Migration[5.1]
   def change
     create_table :spots do |t|
+      t.integer :address_number
+      t.string :number_type
+      t.integer :street_direction
+      t.string :street_name
+      t.string :street_type
 
       t.timestamps
     end
