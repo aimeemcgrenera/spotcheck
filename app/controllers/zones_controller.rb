806a5705a1f1cee0facc05c815d6da16e3f61080
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-22 14:36:47 -0500

    zone controller getting data based on streetname

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,13 +1,17 @@
 class ZonesController < ApplicationController
   def index
-    q = params[:q]
+    odd_even = params[:odd_even]
+    street_direction = params[:street_direction]
+    street_name = params[:street_name]
+    street_type = params[:street_type]
 
-    if q.blank?
-      render status: 400, json: { error: 'Expected parameter `q` '}
+
+    if street_name.blank?
+      render status: 400, json: { error: 'Expected a street parameter '}
     else
       render(
         status: 200,
-        json: Zone.where(["zone", "%#{q}%"]).limit(100)
+        json: Zone.where(:street_name => ["street_name = ?", street_name])
       )
     end
   end
