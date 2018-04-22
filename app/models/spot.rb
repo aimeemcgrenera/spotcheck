3c7b6a92503fdd23e62edd275ed9bae3294de098
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-22 11:17:41 -0500

    added validations for spot and methods in model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,20 @@
 class Spot < ApplicationRecord
+  validates :address_number, :street_direction, :street_name, :street_type, presence: true
+  validates :street_direction, inclusion: { in: %w(N S E W),
+    message: "%{value} is not a valid direction, please enter N S E or W" }
+  before_validation :uppercase
+
+    #street types: AVE, BLVD, ST, PL, TER, RD, PKWY, DR, CT, LN, WAY
+
+    def uppercase
+      self.street_name.upcase!
+    end
+
+    def even?
+      even_odd = self.address_number.even? ? "E" : "O"
+    end
+
+    def numberRange(address_number)
+    address_number >= Zone.address_range_low && address_number <= Zone.address_range_high ? "Permit Zone" : "Not a Permit Zone"
+    end
 end
