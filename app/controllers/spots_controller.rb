0b028fb63b86fee05c25b26ffbf1abb75430c070
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-22 11:27:00 -0500

    added spots controller actions

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,40 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: [:show, :update, :destroy]
+
+  def index
+    @spots = Spot.all
+    render json: @spots
+  end
+
+  def show
+    render json: @spot
+  end
+
+  def create
+    @spot = Spot.new(spot_params)
+    @spot.save!
+    render json: @spot, status: 201
+  end
+
+  def destroy
+    if @spot.destroy!
+      render json: { success_message: 'Successfully deleted!' }, status: 200
+    end
+  end
+
+  def update
+    @spot.update!(spot_params)
+    render json: @spot
+  end
+
+  private
+
+    def set_spot
+      @spot = Spot.find(params[:id])
+    end
+
+    def spot_params
+      params.require(:spot).permit(:address_number, :street_direction, :street_name, :street_type)
+    end
+
 end
