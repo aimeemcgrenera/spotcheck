167c17b9c03bd76ffa675ea7190fcf42a8e4f891
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-22 11:16:37 -0500

    generate spot resource

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,2 +1,14 @@
 class ZonesController < ApplicationController
+  def index
+    q = params[:q]
+
+    if q.blank?
+      render status: 400, json: { error: 'Expected parameter `q` '}
+    else
+      render(
+        status: 200,
+        json: Zone.where(["addressnumber LIKE ?", "%#{q}%"]).limit(100)
+      )
+    end
+  end
 end
