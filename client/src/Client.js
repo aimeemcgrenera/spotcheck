9ee65e6f3dcfa34166e1dc038e3d5f888aa06b5f
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-04 15:35:55 -0500

    addes search function in client

--- client/src/Client.js
+++ client/src/Client.js
@@ -1,2 +1,9 @@
+function search(query) {
+  return fetch(`api/zone?street_name=${query}`, {
+    accept: 'application/json',
+  }).then(checkStatus)
+    .then(parseJSON);
+}
+
 const Client = { search };
 export default Client;
