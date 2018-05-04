0def5d1bb2751d42bbd4b8c23f2ae64e6f8491e0
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-04 11:28:18 -0500

    added app.js and ZoneList.js skeleton

---
+++ client/src/ZoneList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ZoneList = (zones) => {
+  function listZones() {
+    return zones.map(zone => {
+      return (
+        <div className="col-lg-12">
+          <div className="col-lg-6 col-lg-offset-3 well">
+
+          </div>
+        </div>
+      )
+    })
+  }
+
+  return(
+    <div>
+      {listZones()}
+    </div>
+  )
+}
+
+export default ZoneList
