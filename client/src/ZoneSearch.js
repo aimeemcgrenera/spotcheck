015831ed0c5c510ee05d164fc0286d7a8806b078
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-05-08 19:12:18 +0000

    changed search back to Client.js

--- client/src/ZoneSearch.js
+++ client/src/ZoneSearch.js
@@ -2,68 +2,49 @@ import React from 'react';
 import Client from './Client';
 
 class ZoneSearch extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       value: '',
-      zones: [],
+      zones: []
     };
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange = (e) => {
-    this.setState({value: e.target.value});
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
-  handleSearch = (e) => {
-    e.preventDefault()
+  handleSubmit(event) {
+    event.preventDefault()
 
-    const value = e.target.value;
+    alert(this.state.value)
+    const value = this.state.value
+
     this.setState({
       value: value,
     });
 
-    if (value === ''){
+    Client.search(value, (zones) => {
       this.setState({
-        zones:[],
-      });
-    }
-
-    fetch(`api/zone?street_name=${value}`)
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      let zones = data.results.map((zone) => {
-        return(
-          <tr>
-            <td>{zone.status}</td>
-            <td>{zone.zone}</td>
-            <td>{zone.odd_even}</td>
-            <td>{zone.address_range_low}</td>
-            <td>{zone.address_range_high}</td>
-            <td>{zone.street_direction}</td>
-            <td>{zone.street_name}</td>
-            <td>{zone.street_type}</td>
-          </tr>
-        )
+        zones: zones
       })
-      this.setState({zones: zones});
-      console.log("state, this.state.pictures")
-    })
+    });
   }
 
-    render(){
-      return(
-        <div id='zone-search'>
-          <form onSubmit={this.handleSearch} >
-            <input
-            type="text"
-            placeholder="Search Zones..."
-            value={this.state.value}
-            onChange={this.handleChange}
-            />
-            <input type="submit" value="Search" ></input>
-          </form>
+render(){
+  return(
+    <div id='zone-search'>
+      <form onSubmit={this.handleSubmit} >
+        <input
+          type="text"
+          placeholder="Search Zones..."
+          value={this.state.value}
+          onChange={this.handleChange}
+          />
+          <input type="submit" value="Search" ></input>
+        </form>
           {this.state.zones}
         </div>
       )
