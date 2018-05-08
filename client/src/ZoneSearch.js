da9b7e8601c8446f37d4fef94a74f819bc584986
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-05-08 17:18:49 +0000

    adjusted ZoneSearch to not use Client.js

--- client/src/ZoneSearch.js
+++ client/src/ZoneSearch.js
@@ -2,22 +2,20 @@ import React from 'react';
 import Client from './Client';
 
 class ZoneSearch extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       value: '',
       zones: [],
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSearch.bind(this);
   }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({value: e.target.value});
   }
 
-  handleSearch(e){
+  handleSearch = (e) => {
     e.preventDefault()
 
     const value = e.target.value;
@@ -31,28 +29,30 @@ class ZoneSearch extends React.Component {
       });
     }
 
-    Client.search(value => {
-      this.setState({
-        zones: value
+    fetch(`api/zone?street_name=${value}`)
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let zones = data.results.map((zone) => {
+        return(
+          <tr>
+            <td>{zone.status}</td>
+            <td>{zone.zone}</td>
+            <td>{zone.odd_even}</td>
+            <td>{zone.address_range_low}</td>
+            <td>{zone.address_range_high}</td>
+            <td>{zone.street_direction}</td>
+            <td>{zone.street_name}</td>
+            <td>{zone.street_type}</td>
+          </tr>
+        )
       })
+      this.setState({zones: zones});
+      console.log("state, this.state.pictures")
     })
-    debugger
   }
 
     render(){
-      const zoneRows = this.state.zones.map((zone) => (
-        <tr>
-          <td>{zone.status}</td>
-          <td>{zone.zone}</td>
-          <td>{zone.odd_even}</td>
-          <td>{zone.address_range_low}</td>
-          <td>{zone.address_range_high}</td>
-          <td>{zone.street_direction}</td>
-          <td>{zone.street_name}</td>
-          <td>{zone.street_type}</td>
-        </tr>
-      ))
-
       return(
         <div id='zone-search'>
           <form onSubmit={this.handleSearch} >
@@ -64,7 +64,7 @@ class ZoneSearch extends React.Component {
             />
             <input type="submit" value="Search" ></input>
           </form>
-          {zoneRows}
+          {this.state.zones}
         </div>
       )
   }
