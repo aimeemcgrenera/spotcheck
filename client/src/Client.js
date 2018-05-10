8dc615c800c289d17310fe2b7dbab0dac2cf0fdf
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-09 19:25:12 -0500

    zone fetch returning datagit add .

--- client/src/Client.js
+++ client/src/Client.js
@@ -4,26 +4,14 @@ function search(odd_even, direction, street, type) {
   return fetch(`api/zone?odd_even=${odd_even}&street_direction=${direction}&street_name=${street}&street_type=${type}`, {
     method: 'GET',
     accept: 'application/json',
-  }).then(checkStatus) //.then(results => results.json())
-    .then(parseJSON)
+  }).then(results => parseJSON(results))
     .then((data) => {
       console.log('Check this JSON!', data);
     }).catch(err => {throw err})
 }
 
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  const error = new Error(`HTTP Error ${response.statusText}`);
-  error.status = response.statusText;
-  error.response = response;
-  console.log(error);
-  throw error;
-}
-
 function parseJSON(response) {
-  response.json();
+  return response.json();
 }
 
 const Client = { search };
