5b073c76e90dec8e0ee7a3e539c2eaf8d8694ece
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-14 11:23:59 -0500

    adjusted zone index, fetch is working for 3rd party api

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,13 +1,12 @@
 class ZonesController < ApplicationController
   def index
-    # odd_even = params[:odd_even]
-    # street_direction = params[:street_direction]
-    # street_name = params[:street_name]
-    # street_type = params[:street_type]
-    # 
-    # results = Zone.getData(odd_even, street_direction, street_name, street_type)
-    #
-    # render :json => results
+    odd_even = params[:odd_even]
+    street_direction = params[:street_direction]
+    street_name = params[:street_name]
+
+    results = Zone.getData(odd_even, street_direction, street_name)
+
+    render :json => results
 
   end
 end
--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,10 +1,9 @@
 class Zone < ActiveRecord::Base
-  def self.getData(odd_even, street_direction, street_name, street_type)
+  def self.getData(odd_even, street_direction, street_name)
     resp = Faraday.get 'https://data.cityofchicago.org/resource/ys7w-i4tk.json' do |req|
       req.params['odd_even'] = odd_even
       req.params['street_direction'] = street_direction
       req.params['street_name'] = street_name
-      req.params['street_type'] = street_type
       req.params['status'] = "ACTIVE"
     end
     body_hash = JSON.parse(resp.body)
