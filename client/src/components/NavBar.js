94c6011fc65cda7025c3ccae1182e4a48524187b
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-15 16:38:36 -0500

    adjusted home page and navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Home = () => {
+const NavBar = () => {
   return (
-    <div className="HomePage">
-      <h2>Welcome to SpotCheck</h2>
-      <p></p>
+    <div className="NavBar">
+      <NavLink  className="NavLink" to="/">Home</NavLink>
+      <NavLink  className="NavLink" to="/spotnew">Check Spot</NavLink>
+      <NavLink  className="NavLink" to="/spots">Previous Spots</NavLink>
+      <NavLink  className="NavLink" to="/zones">All Zones</NavLink>
     </div>
   );
 };
 
-export default Home;
+export default NavBar;
