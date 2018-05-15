356ebea5dd71a05def14e5917626b89ac5e47b50
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-15 15:55:20 -0500

    have zones populating

--- client/src/ZoneList.js
+++ client/src/ZoneList.js
@@ -3,11 +3,11 @@ import ZoneSearch from './ZoneSearch';
 import SpotNew from './SpotNew';
 
 function ZoneList(props) {
-  const zones = props.map((zone) =>
+  const zones = props.zones.map((zone) =>
     <div key={zone.id}>
-      <h3>{zone.zone}</h3>
-      <p>{zone.address_range_low} " - " {zone.address_range_high}</p>
-      <p>{zone.street_direction}{zone.street_name}</p>
+      <h3>Zone number: {zone.zone}</h3>
+      <p>Number Range: {zone.address_range_low}  -  {zone.address_range_high}</p>
+      <p>Street: {zone.street_direction} {zone.street_name}</p>
     </div>
   );
   return(
